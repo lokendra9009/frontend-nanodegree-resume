8a79f772b31583ea9fce04c3082b3f3b73aee72c
Lokendra Singh Chouhan <lokendra91@gmail.com>
2016-12-19 19:31:10 +0530

    linted the json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var formattedName = HTMLheaderName.replace('%data%','Lokendra Singh Chouhan');
+/*var formattedName = HTMLheaderName.replace('%data%','Lokendra Singh Chouhan');
 var formattedRole = HTMLheaderRole.replace("%data%",'Quality Assurance Engineer');
 
 $('#header').prepend(formattedRole);
@@ -29,3 +29,71 @@ education["city"]="";
 
 $("#main").prepend(bio.name);
 $('#main').append(bio.city);
+*/
+var education = {
+	"schools": [{
+		"name": "Acropolis Institute of Technology and Research",
+		"city": "Indore, Madhya Pradesh, India",
+		"degree": "Bachelor Of Engineering",
+		"major": ["Computer Science"]
+	}, {
+		"name": "Emerald Heights",
+		"city": "Indore, Madhya Pradesh, India",
+		"degree": "AISSE",
+		"major": ["Physics", "Chemistry", "Maths"]
+	}, {
+		"name": "Emerald Heights",
+		"city": "Indore, Madhya Pradesh, India",
+		"degree": "AISSCE",
+		"major": ["Maths, French"]
+	}],
+	"Online Courses": [
+		{
+			"title":"Javascript Basics",
+			"institute":"udacity",
+			"dates":"2016",
+			"url":"http://www.udacity.com"
+		}
+	]
+};
+
+var bio = {
+	"name": "Lokendra Singh Chouhan",
+	"role": "Quality Assurance",
+	"welcomeMessage": "Hello World!",
+	"biopic": "url",
+	"contact": [{
+		"mobile": "+91-9552528557"
+	}, {
+		"email": "lokendra91@gmail.com"
+	}, {
+		"github": "url"
+	}, {
+		"linkedin": "linkedin page"
+	}],
+	"skills": ["Selenium Webdriver", "programming", "testing"]
+
+};
+
+var work = {
+	"jobs": [{
+		"employer": "Imagination Technologies",
+		"title": "Graduate Test Engineer",
+		"location": "Pune",
+		"dates": "20-Sept-2015 to Present",
+		"description": "abc"
+	}, {
+		"employer": "Amdocs Inc.",
+		"title": "Software Test Engineer",
+		"location": "Pune",
+		"dates": "01-July-2013 to 10-July-2015",
+		"description": "xyz"
+	}]
+
+};
+
+var project = {
+
+	
+};
+
